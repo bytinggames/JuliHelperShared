500169c6002f2ced1551f79c8953fb0278fbd185
bytinggames <bytinggames@gmail.com>
2020-10-04 17:51:06 +0200

    [master]: TextureAnim

--- JuliHelperShared/Content/ContentFenja.cs
+++ JuliHelperShared/Content/ContentFenja.cs
@@ -4,8 +4,10 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using JuliHelperShared;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -15,24 +17,44 @@ namespace JuliHelper
     {
         private static Type[] GetNestedTypes(Type ofType) => ofType.GetNestedTypes();
 
+
         public static void LoadRaw(Type fieldContainingClass, string contentPath, string localPath, GraphicsDevice gDevice)
         {
             var fields = fieldContainingClass.GetFields(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+
+
+            string GetPath(string fileName)
+            {
+                return Path.Combine(contentPath, localPath, fileName);
+            }
+
+            Texture2D GetTexture(string fileName)
+            {
+                string path = GetPath(fileName);
+                if (!File.Exists(path))
+                {
+                    string[] files = System.IO.Directory.GetFiles(System.IO.Path.GetDirectoryName(path), fileName + "#*.png");
+                    if (files.Length != 1)
+                        throw new Exception(files.Length + " matching files found for " + fileName);
+
+                    path = files[0];
+                }
+
+                using (FileStream stream = new FileStream(path, FileMode.Open))
+                {
+                    Texture2D newTex = Texture2D.FromStream(gDevice, stream);
+                    newTex.Name = Path.Combine(localPath, fileName);
+                    return newTex;
+                }
+            }
+            
 
             var switchType = new Dictionary<Type, Action<FieldInfo>>
             {
                 {
                     typeof(Texture2D), f =>
                     {
-                        Texture2D newTex;
-
-                        string path = Path.Combine(contentPath, localPath, f.Name + ".png");
-                        //FileInfo info = new FileInfo(path);
-                        using (FileStream stream = new FileStream(path, FileMode.Open))
-                        {
-                            newTex = Texture2D.FromStream(gDevice, stream);
-                            newTex.Name = Path.Combine(localPath, f.Name);
-                        }
+                        Texture2D newTex = GetTexture(f.Name + ".png");
 
                         Texture2D tex = f.GetValue(null) as Texture2D;
 
@@ -52,26 +74,13 @@ namespace JuliHelper
                     {
                         int index = 0;
 
-                        string GetPath()
-                        {
-                            return Path.Combine(contentPath, localPath, f.Name + "_" + index + ".png");
-                        }
-
-                        string path = GetPath();
-
-                        if (!File.Exists(path))
-                            index++;
-
                         List<Texture2D> newTexs = new List<Texture2D>();
 
-                        while (File.Exists(path = GetPath()))
+                        string GetFileName() => f.Name + "_" + index + ".png";
+
+                        while (File.Exists(GetPath(GetFileName())))
                         {
-                            using (FileStream stream = new FileStream(path, FileMode.Open))
-                            {
-                                Texture2D tex = Texture2D.FromStream(gDevice, stream);
-                                tex.Name = Path.Combine(localPath, f.Name + "_" + index);
-                                newTexs.Add(tex);
-                            }
+                            newTexs.Add(GetTexture(GetFileName()));
                             index++;
                         }
 
@@ -86,13 +95,81 @@ namespace JuliHelper
                             for (int i = 0; i < texs.Count; i++)
                             {
                                 texs[i].SetData(newTexs[i].ToColor());
-			                }
+                            }
                         }
                     }
-                }
+                },
+                {
+                    typeof(TextureAnim), f =>
+                    {
+                        Texture2D newTex = GetTexture(f.Name + ".png");
+
+                        TextureAnim newAni;
+                        string aniPath = GetPath(f.Name + ".ani");
+                        if (!File.Exists(aniPath))
+                        {
+                            newAni = new TextureAnim(newTex);
+                        }
+                        else
+                        {
+                            newAni = new TextureAnim(newTex, File.ReadAllLines(aniPath));
+                        }
+
+                        TextureAnim ani = f.GetValue(null) as TextureAnim;
+
+                        // is a texture already loaded?
+                        if (ani == null)
+                        {
+                            f.SetValue(null, newAni);
+                        }
+                        else
+                        {
+                            newAni.CopySettingsTo(ani);
+                        }
+                    }
+                },
+                {
+                    typeof(TextureAnim[]), f =>
+                    {
+                        int index = 0;
+
+                        List<Texture2D> newTexs = new List<Texture2D>();
+
+                        string GetFileName() => f.Name + "_" + index + ".png";
+
+                        while (File.Exists(GetPath(GetFileName())))
+                        {
+                            newTexs.Add(GetTexture(GetFileName()));
+                            index++;
+                        }
+
+                        List<TextureAnim> newAnims = new List<TextureAnim>();
+
+                        for (int i = 0; i < newTexs.Count; i++)
+                        {
+                            string aniPath = GetPath(f.Name + "_" + i + ".ani");
+                            if (!File.Exists(aniPath))
+                                newAnims.Add(new TextureAnim(newTexs[i]));
+                            else
+                                newAnims.Add(new TextureAnim(newTexs[i], File.ReadAllLines(aniPath)));
+                        }
+
+                        List<TextureAnim> anims = (f.GetValue(null) as TextureAnim[])?.ToList();
+                        if (anims == null)
+                        {
+                            f.SetValue(null, newAnims.ToArray());
+                        }
+                        else
+                        {
+                            for (int i = 0; i < anims.Count; i++)
+                            {
+                                newAnims[i].CopySettingsTo(anims[i]);
+                            }
+                        }
+                    }
+                },
             };
 
-            // RELEASE: replace this with hard code
             for (int i = 0; i < fields.Length; i++)
             {
                 if (switchType.ContainsKey(fields[i].FieldType))
@@ -124,8 +201,6 @@ namespace JuliHelper
                         List<Texture2D> texs = new List<Texture2D>();
                         int index = 0;
                         string name = f.Name + "_" + index;
-                        if (!File.Exists(Path.Combine("Content", localPath,name+".xnb")))
-                            index++;
 
                         while (File.Exists(Path.Combine("Content", localPath,(name = f.Name + "_"+index)+".xnb")))
                         {
---
+++ JuliHelperShared/Content/TextureAnim.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JuliHelper;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace JuliHelperShared
+{
+    public class TextureAnim
+    {
+        public Texture2D Texture { get; set; }
+        public int FrameWidth { get; set; }
+        public int FrameHeight { get; set; }
+        public int FrameCount { get; set; }
+        public float Fps { get; set; }
+
+        public TextureAnim(Texture2D tex)
+        {
+            Initialize(tex);
+
+        }
+        public TextureAnim(Texture2D tex, string[] aniContent)
+        {
+            FrameCount = FrameHeight = FrameWidth = -1;
+
+            for (int i = 0; i < aniContent.Length; i++)
+            {
+                string[] split = aniContent[i].Split(new char[] { '=' }, StringSplitOptions.RemoveEmptyEntries);
+                switch (split[0])
+                {
+                    case "w":
+                        FrameWidth = Convert.ToInt32(split[1]);
+                        break;
+                    case "h":
+                        FrameHeight = Convert.ToInt32(split[1]);
+                        break;
+                    case "count":
+                        FrameCount = Convert.ToInt32(split[1]);
+                        break;
+                    case "fps":
+                        Fps = Convert.ToSingle(split[1]);
+                        break;
+                }
+            }
+            Initialize(tex, FrameWidth, FrameHeight, FrameCount);
+        }
+
+        public Rectangle GetFrameRect(int frame)
+        {
+            frame %= FrameCount;
+            if (frame < 0)
+                frame += FrameCount;
+
+            int framesInX = Texture.Width / FrameWidth;
+
+            int x = frame % framesInX;
+            int y = frame / framesInX;
+
+            return new Rectangle(x * FrameWidth, y * FrameHeight, FrameWidth, FrameHeight);
+        }
+
+        public TextureAnim(Texture2D tex, int frameWidth, int frameHeight)
+        {
+            Initialize(tex, frameWidth, frameHeight);
+        }
+
+        public TextureAnim(Texture2D tex, int frameWidth, int frameHeight, int frameCount)
+        {
+
+            Initialize(tex, frameWidth, frameHeight, frameCount);
+        }
+
+        private void Initialize(Texture2D _tex, int _frameWidth = -1, int _frameHeight = -1, int _frameCount = -1)
+        {
+            Texture = _tex;
+
+            if (_frameWidth != -1)
+                FrameWidth = _frameWidth;
+            else
+            {
+                FrameWidth = Texture.Height;
+
+                if (Texture.Width / FrameWidth * FrameWidth != Texture.Width)
+                    throw new Exception($"Texture should be {Texture.Width / FrameWidth * FrameWidth} pixels wide, but is {Texture.Width} pixels wide");
+            }
+            if (_frameHeight != -1)
+                FrameHeight = _frameHeight;
+            else
+            {
+                FrameHeight = Texture.Height;
+                if (Texture.Height / FrameHeight * FrameHeight != Texture.Height)
+                    throw new Exception($"Texture should be {Texture.Height / FrameHeight * FrameHeight} pixels high, but is {Texture.Height} pixels high");
+            }
+
+            if (_frameCount != -1)
+                FrameCount = _frameCount;
+            else
+            {
+                FrameCount = (Texture.Width / FrameWidth) * (Texture.Height / FrameHeight);
+            }
+        }
+
+        public void CopySettingsTo(TextureAnim ani)
+        {
+            ani.Texture.SetData(Texture.ToColor());
+            ani.FrameWidth = FrameWidth;
+            ani.FrameHeight = FrameHeight;
+            ani.FrameCount = FrameCount;
+        }
+    }
+
+}
