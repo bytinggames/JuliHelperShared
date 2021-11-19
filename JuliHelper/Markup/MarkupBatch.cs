0a044cb45a4780715e3a08d2b77d1927603e1edd
bytinggames <bytinggames@gmail.com>
2021-11-19 16:26:51 +0100

    [master]: MarkupBatch

---
+++ JuliHelper/Markup/MarkupBatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JuliHelper.Markup
+{
+    class MarkupBatch
+    {
+        public MarkupRoot Root { get; }
+        public MarkupSettings Settings { get; }
+
+        public MarkupBatch(MarkupRoot root, MarkupSettings settings)
+        {
+            Root = root;
+            Settings = settings;
+        }
+
+        public void Draw()
+        {
+            Root.Draw(Settings);
+        }
+    }
+}
